6cf3c8206243a657c92d7d10a024184e40999d6a
Riaan Vos <riaan.vos@blockchain.com>
2016-09-05 15:55:48 +0100

    refactor(API): Refactor transaction API to use local vars. WIP - this doesn't seem to be used anywhere.

--- src/main/java/info/blockchain/api/Transaction.java
+++ src/main/java/info/blockchain/api/Transaction.java
@@ -1,33 +1,10 @@
 package info.blockchain.api;
 
-import info.blockchain.wallet.util.WebUtil;
-import org.json.JSONObject;
-
 public class Transaction {
 
     private static final String PROTOCOL = "https://";
     private static final String SERVER_ADDRESS = "blockchain.info/";
-    public static final String PROD_ADDRESS_INFO_URL = PROTOCOL + SERVER_ADDRESS + "address/";
+    public static final String PROD_TRANSACTION_URL = PROTOCOL + SERVER_ADDRESS+ "tx/";
 
-    public JSONObject getAddressInfo(String address, String parameter) {
-
-        JSONObject jsonObject  = null;
-
-        try {
-            StringBuilder url = new StringBuilder(PROD_ADDRESS_INFO_URL);
-            url.append(address);
-            url.append("?format=json");
-            if(parameter != null && !parameter.isEmpty())
-                url.append(parameter);
-
-            String response = WebUtil.getInstance().getURL(url.toString());
-            jsonObject = new JSONObject(response);
-        }
-        catch(Exception e) {
-            jsonObject = null;
-            e.printStackTrace();
-        }
-
-        return jsonObject;
-    }
+    //TODO - This api might not be used at all yet
 }
