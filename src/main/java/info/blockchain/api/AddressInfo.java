16322f507939237ab4a2ffaf0d2ee48f9e814d0f
Riaan Vos <riaan.vos@blockchain.com>
2016-09-05 15:53:31 +0100

    refactor(API): Refactor address api to separate class.

--- src/main/java/info/blockchain/api/AddressInfo.java
+++ src/main/java/info/blockchain/api/AddressInfo.java
@@ -1,7 +1,33 @@
 package info.blockchain.api;
 
-/**
- * Created by riaanvos on 05/09/16.
- */
+import info.blockchain.wallet.util.WebUtil;
+import org.json.JSONObject;
+
 public class AddressInfo {
+
+    private static final String PROTOCOL = "https://";
+    private static final String SERVER_ADDRESS = "blockchain.info/";
+    public static final String PROD_ADDRESS_INFO_URL = PROTOCOL + SERVER_ADDRESS + "address/";
+
+    public JSONObject getAddressInfo(String address, String parameter) {
+
+        JSONObject jsonObject  = null;
+
+        try {
+            StringBuilder url = new StringBuilder(PROD_ADDRESS_INFO_URL);
+            url.append(address);
+            url.append("?format=json");
+            if(parameter != null && !parameter.isEmpty())
+                url.append(parameter);
+
+            String response = WebUtil.getInstance().getURL(url.toString());
+            jsonObject = new JSONObject(response);
+        }
+        catch(Exception e) {
+            jsonObject = null;
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
 }
