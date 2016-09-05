e9bb923037efeb32bffddeb0c50802097948e7b8
Riaan Vos <riaan.vos@blockchain.com>
2016-09-05 15:54:38 +0100

    refactor(API): Refactor push tx API to use local vars.

--- src/main/java/info/blockchain/api/PushTx.java
+++ src/main/java/info/blockchain/api/PushTx.java
@@ -6,11 +6,15 @@ import org.bitcoinj.core.Transaction;
 
 public class PushTx {
 
+    private static final String PROTOCOL = "https://";
+    private static final String SERVER_ADDRESS = "blockchain.info/";
+    public static final String PROD_SPEND_URL = PROTOCOL + SERVER_ADDRESS + "pushtx";
+
     public String submitTransaction(String hexString) throws Exception {
 
         hexString += "&api_code="+WebUtil.API_CODE;
 
-        String response = WebUtil.getInstance().postURL(WebUtil.SPEND_URL, "tx=" + hexString);
+        String response = WebUtil.getInstance().postURL(PROD_SPEND_URL, "tx=" + hexString);
 
         return response;
     }
@@ -20,7 +24,7 @@ public class PushTx {
         String encoded = SendCoins.getInstance().encodeHex(tx);
         encoded += "&api_code="+WebUtil.API_CODE;
 
-        String response = WebUtil.getInstance().postURL(WebUtil.SPEND_URL, "tx=" + encoded);
+        String response = WebUtil.getInstance().postURL(PROD_SPEND_URL, "tx=" + encoded);
 
         return response;
     }
