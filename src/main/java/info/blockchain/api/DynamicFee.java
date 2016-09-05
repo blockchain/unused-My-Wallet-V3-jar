4ba7983f82383106c16b087a580627aee58281ca
Riaan Vos <riaan.vos@blockchain.com>
2016-09-05 15:54:16 +0100

    refactor(API): Refactor dynamic fee API to use local vars.

--- src/main/java/info/blockchain/api/DynamicFee.java
+++ src/main/java/info/blockchain/api/DynamicFee.java
@@ -11,11 +11,38 @@ import java.util.ArrayList;
 
 public class DynamicFee {
 
+    private static final String PROTOCOL = "https://";
+    private static final String SERVER_ADDRESS = "blockchain.info/";
+
+    private static final String API_SUBDOMAIN = "api.";
+    private static final String DEV_SUBDOMAIN = "dev.";
+    private static final String STAGING_SUBDOMAIN = "staging.";
+
+    private static final String API_DEV_SUBDOMAIN = API_SUBDOMAIN +DEV_SUBDOMAIN;
+    private static final String API_STAGING_SUBDOMAIN = API_SUBDOMAIN +STAGING_SUBDOMAIN;
+
+    private static final String FEES = "fees";
+    public static final String PROD_DYNAMIC_FEE = PROTOCOL + API_SUBDOMAIN + SERVER_ADDRESS + FEES;
+    public static final String DEV_DYNAMIC_FEE = PROTOCOL + API_DEV_SUBDOMAIN + SERVER_ADDRESS + FEES;
+    public static final String STAGING_DYNAMIC_FEE = PROTOCOL + API_STAGING_SUBDOMAIN + SERVER_ADDRESS + FEES;
+
+    private String dynamicFeeUrl = PROD_DYNAMIC_FEE;
+
+    public DynamicFee() {
+    }
+
+    /**
+     * @param customUrl PROD_DYNAMIC_FEE, DEV_DYNAMIC_FEE, STAGING_DYNAMIC_FEE
+     */
+    public DynamicFee(String customUrl) {
+        this.dynamicFeeUrl = customUrl;
+    }
+
     public SuggestedFee getDynamicFee() {
 
         String response = null;
         try {
-            response = WebUtil.getInstance().getURL(WebUtil.DYNAMIC_FEE);
+            response = WebUtil.getInstance().getURL(dynamicFeeUrl);
         } catch (Exception e) {
             e.printStackTrace();
             return getDefaultFee();
