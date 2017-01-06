bbbfb0a4404130e5192d78835196d09fb3a0f715
riaanjvos <riaan.vos@blockchain.com>
2017-01-06 16:49:52 +0000

    fix(Second Password): Check if encryption is consistent before syncing payload with server.

---
+++ src/test/java/info/blockchain/wallet/util/FormatsUtilTest.java
@@ -0,0 +1,26 @@
+package info.blockchain.wallet.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FormatsUtilTest {
+
+    @Test
+    public void isEncrypted() throws Exception {
+
+        Assert.assertTrue(!FormatsUtil.getInstance().isKeyEncrypted(null));
+        Assert.assertTrue(!FormatsUtil.getInstance().isKeyEncrypted("3tcnfpTzY6G6oL4NujXkXJfpkEJr69fDSRESuA76izac"));
+
+        Assert.assertTrue(FormatsUtil.getInstance().isKeyEncrypted("51jTHC6+phVaDTqZOyldKRRqrZQiXm/IhTMAjM/G9eCVQJt6POLTsKQT29RlFH9vH2tbJaowM5firNiSiNNIPw=="));
+        Assert.assertTrue(FormatsUtil.getInstance().isKeyEncrypted("QQBIDa4SO84Uow1AlWo/1STqO2n5OXN6seU2eULjK/4ydHYW/LRTmBQT3eyIgdYCnNtJ1QBSatZ/9d4oNbkH0pmPeZEd+4Sekz9zoqfJs35k0kt7R3De+L6cqYymLpQJLELZwlP78SmWnlC31pCAB/lklBXwlv9xcSRq9qO9sLk="));
+    }
+
+    @Test
+    public void isUnencrypted() throws Exception {
+
+        Assert.assertTrue(!FormatsUtil.getInstance().isKeyUnencrypted(null));
+        Assert.assertTrue(!FormatsUtil.getInstance().isKeyUnencrypted("51jTHC6+phVaDTqZOyldKRRqrZQiXm/IhTMAjM/G9eCVQJt6POLTsKQT29RlFH9vH2tbJaowM5firNiSiNNIPw=="));
+        Assert.assertTrue(!FormatsUtil.getInstance().isKeyUnencrypted("QQBIDa4SO84Uow1AlWo/1STqO2n5OXN6seU2eULjK/4ydHYW/LRTmBQT3eyIgdYCnNtJ1QBSatZ/9d4oNbkH0pmPeZEd+4Sekz9zoqfJs35k0kt7R3De+L6cqYymLpQJLELZwlP78SmWnlC31pCAB/lklBXwlv9xcSRq9qO9sLk="));
+        Assert.assertTrue(FormatsUtil.getInstance().isKeyUnencrypted("3tcnfpTzY6G6oL4NujXkXJfpkEJr69fDSRESuA76izac"));
+    }
+}
