21019d120c11cb11cee298261588579585795601
Adam Bennett <adam@blockchain.com>
2017-08-23 17:10:04 +0100

    feature(Ethereum): Switch from Single to Observable

--- src/main/java/info/blockchain/wallet/ethereum/EthAccountApi.java
+++ src/main/java/info/blockchain/wallet/ethereum/EthAccountApi.java
@@ -5,7 +5,7 @@ import info.blockchain.wallet.ethereum.data.EthAccount;
 
 import java.util.HashMap;
 
-import io.reactivex.Single;
+import io.reactivex.Observable;
 import io.reactivex.functions.Function;
 
 @SuppressWarnings("WeakerAccess")
@@ -14,27 +14,26 @@ public class EthAccountApi {
     private EthEndpoints endpoints;
 
     /**
-     * Returns an {@link EthAccount} object for a given ETH address as a {@link Single}, which is to
-     * say that either the object is returned or an error. An {@link EthAccount} contains a list of
-     * transactions associated with the account, as well as a final balance.
+     * Returns an {@link EthAccount} object for a given ETH address as an {@link Observable}. An
+     * {@link EthAccount} contains a list of transactions associated with the account, as well as a
+     * final balance.
      *
      * @param address The ETH address to be queried
-     * @return A {@link Single} wrapping an {@link EthAccount}
+     * @return An {@link Observable} wrapping an {@link EthAccount}
      */
-    public Single<EthAccount> getEthAccount(String address) {
+    public Observable<EthAccount> getEthAccount(String address) {
         return getApiInstance().getEthAccount(address);
     }
 
     /**
      * Returns true if a given ETH address is associated with an Ethereum contract, which is
      * currently unsupported. This should be used to validate any proposed destination address for
-     * funds. The returned object is a {@link Single}, which is to say that either a boolean is
-     * returned or an error.
+     * funds.
      *
      * @param address The ETH address to be queried
-     * @return A {@link Single} returning true or false based on the address's contract status
+     * @return An {@link Observable} returning true or false based on the address's contract status
      */
-    public Single<Boolean> getIfContract(String address) {
+    public Observable<Boolean> getIfContract(String address) {
         return getApiInstance().getIfContract(address)
                 .map(new Function<HashMap<String, Boolean>, Boolean>() {
                     @Override
--- src/main/java/info/blockchain/wallet/ethereum/EthEndpoints.java
+++ src/main/java/info/blockchain/wallet/ethereum/EthEndpoints.java
@@ -4,16 +4,16 @@ import info.blockchain.wallet.ethereum.data.EthAccount;
 
 import java.util.HashMap;
 
-import io.reactivex.Single;
+import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 interface EthEndpoints {
 
     @GET(EthUrls.ACCOUNT + "/{address}")
-    Single<EthAccount> getEthAccount(@Query("address") String address);
+    Observable<EthAccount> getEthAccount(@Query("address") String address);
 
     @GET(EthUrls.ACCOUNT + "/{address}" + EthUrls.IS_CONTRACT)
-    Single<HashMap<String, Boolean>> getIfContract(@Query("address") String address);
+    Observable<HashMap<String, Boolean>> getIfContract(@Query("address") String address);
 
 }
