4eb520451133f1a69d3ddcfc008fa000fbc229e1
riaanjvos <riaan.vos@blockchain.com>
2017-09-04 11:30:39 +0100

    feature(Ethereum): Add ETH fee options and test.

--- src/test/java/info/blockchain/wallet/api/FeeApiTest.java
+++ src/test/java/info/blockchain/wallet/api/FeeApiTest.java
@@ -30,4 +30,24 @@ public class FeeApiTest extends MockedResponseTest {
         Assert.assertEquals(396, feeOptionsList.getLimits().getMax());
         Assert.assertEquals(98, feeOptionsList.getLimits().getMin());
     }
+
+    @Test
+    public void getEthFeeOptions() throws Exception {
+
+        URI uri = getClass().getClassLoader().getResource("fee_eth_options.txt").toURI();
+        String feeOptions = new String(Files.readAllBytes(Paths.get(uri)), Charset.forName("utf-8"));
+
+        mockInterceptor.setResponseString(feeOptions);
+        final TestObserver<FeeOptions> testObserver = new FeeApi().getEthFeeOptions().test();
+
+        testObserver.assertComplete();
+        testObserver.assertNoErrors();
+        FeeOptions feeOptionsList = testObserver.values().get(0);
+
+        Assert.assertEquals(21000, feeOptionsList.getGasLimit());
+        Assert.assertEquals(21, feeOptionsList.getPriorityFee());
+        Assert.assertEquals(21, feeOptionsList.getRegularFee());
+        Assert.assertEquals(21, feeOptionsList.getLimits().getMax());
+        Assert.assertEquals(21, feeOptionsList.getLimits().getMin());
+    }
 }
